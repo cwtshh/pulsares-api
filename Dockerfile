8fb8dc6c4155a5bbd430b6176eae88e00803c383
Gabriel Oliveira Moura Lima <qualifiedwind@gmail.com>
2024-10-17 07:53:11 -0400

    corringindo dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,9 @@ WORKDIR /app
 
 COPY requirements.txt /app/
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install -y ffmpeg
 
-RUN sudo apt update \
-    sudo apt install ffmpeg
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
+from moviepy.editor import VideoFileClip
 from fastapi.responses import JSONResponse
 import shutil
 import os
+import whisper
 
 app = FastAPI()
 
@@ -20,5 +22,13 @@ async def upload_video(file: UploadFile = File(...)):
         f.write(await file.read())
     
     return JSONResponse(content={"filename": file.filename})
+
+
+def video_to_audio(file_location):
+    video = VideoFileClip(file_location)
+    audio = video.audio
+    audio_file_location = file_location.rsplit('.', 1)[0] + '.wav'
+    audio.write_audiofile(audio_file_location)
+    return audio_file_location
 
 
