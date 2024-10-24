d5314ae887b39479d0429515e056b5f7aa245b2d
Gabriel Oliveira Moura Lima <qualifiedwind@gmail.com>
2024-10-24 11:28:07 -0400

    implmentação do faster whisper

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import shutil
 import os
+from faster_whisper import WhisperModel as whisper
 import subprocess
-import whisper
 import time
 
 app = FastAPI()
@@ -31,27 +31,33 @@ def test_route():
 @app.post("/upload-video/")
 async def upload_video(file: UploadFile = File(...)):
     file_location = f"uploads/{file.filename}"
-    # Salvar o arquivo no disco
+    # Save the file to disk
     with open(file_location, "wb") as f:
         f.write(await file.read())
 
-    start_time = time.time()  # Iniciar o temporizador
+    start_time = time.time()  # Start the timer
     audio_path = convert_video_to_wav(file_location)
-    conversion_time = time.time() - start_time  # Calcular tempo de conversão
+    conversion_time = time.time() - start_time  # Calculate conversion time
 
-    start_time = time.time()  # Reiniciar o temporizador
+    start_time = time.time()  # Restart the timer
     result = transcribe_audio_with_stamps(audio_path)
-    transcription_time = time.time() - start_time  # Calcular tempo de transcrição
+    transcription_time = time.time() - start_time  # Calculate transcription time
 
-    # deletar arquivos temporários
+    # Delete temporary files
     os.remove(file_location)
     os.remove(audio_path)
+
+    # Convert the result to a list if it is a generator
+    if isinstance(result, (list, tuple)):
+        result_list = result
+    else:
+        result_list = list(result)
 
     return JSONResponse(content={
         "filename": file.filename,
         "conversion_time": conversion_time,
         "transcription_time": transcription_time,
-        "result": result
+        "result": result_list
     })
 
 def convert_video_to_wav(video_path, output_path=None):
@@ -70,10 +76,14 @@ def convert_video_to_wav(video_path, output_path=None):
 
 def transcribe_audio_with_stamps(audio_path):
     # Carregar o modelo uma vez
-    model = whisper.load_model("small")
-    
+    model_size = "small"
+    model = whisper(model_size, device="cpu", compute_type="int8")
+
     # Transcrever o áudio usando o modelo carregado
-    result = whisper.transcribe(audio=audio_path, model=model)
-    
-    return result
+    result, info = model.transcribe(audio_path, beam_size=5)
+
+    # Convert the generator to a list
+    result_list = list(result)
+
+    return result_list
 
